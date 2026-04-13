40076a18207e31ec5c4cc6ed8b24dfbad6f29bba
LuisSousa3 <luissousa.mief@gmail.com>
2026-04-13 11:18:31 +0100

    Import code from ssms_event_cameras

--- main.py
+++ main.py
@@ -17,3 +17,6 @@ python split_gen4_into_60s.py
 python split_gen4_60s_train_eval_test.py
 python3 scripts/data/psee_to_frames.py data/gen4 data/gen1/gen1_preproc_npz -n 10 --mangling rvt-gen1 -d 50ms --workers 30
 """
+
+
+
